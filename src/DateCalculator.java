63f053bb8eb2f8873742e63eeb55c1075545552e
barredel <bar.redel@campus.technion.ac.il>
2022-06-27 13:45:58 +0300

    Initial commit

---
+++ src/DateCalculator.java
@@ -0,0 +1,9 @@
+public class DateCalculator
+{
+    public class DateCalculator {
+        public static Date addToDate(Date date, int num) {
+            // TODO: Add your code here...
+        }
+    }
+
+}
---
+++ src/Lock.java
@@ -0,0 +1,9 @@
+public class Lock
+{
+    public interface Lock extends AutoCloseable {
+        void acquire();
+        boolean tryAcquire();
+        void release();
+    }
+
+}
