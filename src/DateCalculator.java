e2b8a1dabc50246e123847678bde7bd18d5000ff
barredel <bar.redel@campus.technion.ac.il>
2022-06-27 17:54:40 +0300

    1

--- src/DateCalculator.java
+++ src/DateCalculator.java
@@ -1,6 +1,17 @@
-public class DateCalculator {
 
-    public static Date addToDate(Date date, int num) {
-        // TODO: Add your code here...
+public class DateCalculator
+{
+    public class DateCalculator
+    {
+        public static Date addToDate(Date date, int num)
+        {
+            // TODO: Add your code here...
+        }
+
+        public boolean isLeapYear(Date date)
+        {
+            if (date.getYear())
+        }
     }
+
 }
