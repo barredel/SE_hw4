c15245feb7e462e27ee6a9c26251bccbfa27799d
barredel <bar.redel@campus.technion.ac.il>
2022-06-28 18:01:36 +0300

    1

--- src/LevelMostOccurrences.java
+++ src/LevelMostOccurrences.java
@@ -1,6 +1,14 @@
-public class LevelMostOccurrences {
-    public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num) {
-        // TODO: Add your code here...
+ import  java.util.ArrayDeque;
+import java.util.Queue;
+
+ public class LevelMostOccurrences {
+    public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num)
+    {// TODO: Add your code here...
+        Queue<Object> perents = new ArrayDeque<>();
+        Queue<Object> kids = new ArrayDeque<>();
+
+        while()
+
         int i = 0;
         i++;
         return i;
