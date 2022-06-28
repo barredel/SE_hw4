2be6045be4004371e3fb65820354c323c39fa8fc
barredel <bar.redel@campus.technion.ac.il>
2022-06-28 22:16:25 +0300

    1

--- src/LevelMostOccurrences.java
+++ src/LevelMostOccurrences.java
@@ -3,14 +3,47 @@ import java.util.Queue;
 
  public class LevelMostOccurrences {
     public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num)
-    {// TODO: Add your code here...
-        Queue<Object> perents = new ArrayDeque<>();
-        Queue<Object> kids = new ArrayDeque<>();
+    {
+        Queue<BinNode<Integer>> parents = new ArrayDeque<>();
+        Queue<BinNode<Integer>>  kids = new ArrayDeque<>();
+        int levelMostOccurrences = -1;
+        int currentLevel = 0;
+        int maxOccurrences=0;
+        int levelOccurrences = 0;
 
-        while()
+        parents.add(node);
+        BinNode<Integer> data;
+        while(!parents.isEmpty())
+        {
+            data = parents.poll();
+            if(data.getData() == num)
+            {
+                levelOccurrences++;
+            }
+            if (data.getRight()!=null)
+            {
+                kids.add(data.getRight());
+            }
+            if(data.getLeft()!=null)
+            {
+                kids.add(data.getLeft());
+            }
+            if(levelOccurrences>maxOccurrences)
+            {
+                maxOccurrences = levelOccurrences;
+                levelMostOccurrences = currentLevel;
+            }
+            if(parents.isEmpty())
+            {
+                currentLevel++;
+                levelOccurrences = 0;
+                while (!kids.isEmpty())
+                {
+                    parents.add(kids.poll());
+                }
+            }
+        }
 
-        int i = 0;
-        i++;
-        return i;
+        return levelMostOccurrences;
     }
 }
