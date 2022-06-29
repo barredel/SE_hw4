46a68d5aa508dd12ea928cc1fc037c0bda02da15
Bar <bar.muller@campus.technion.ac.il>
2022-06-29 19:10:02 +0300

    e

--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -1,67 +1,86 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * a class represents a reentrant lock
+ */
 public class MyReentrantLock implements Lock{
-    private AtomicBoolean is_locked;
+    private AtomicBoolean isLocked;
     private Thread lockingThread;
     private int counter;
 
+    /**
+     * class constructor
+     */
     public MyReentrantLock()
     {
-        this.is_locked = new AtomicBoolean(false);
+        this.isLocked = new AtomicBoolean(false);
         this.counter = 0;
         this.lockingThread = Thread.currentThread();
     }
 
+    /**
+     * if the lock isn't locked, locks it. if the locking thread is the one trying to lock again, allows it
+     * if another thread tries to lock it, puts it to sleep
+     * @throws InterruptedException if unable to put a thread to sleep
+     */
     @Override
     public void acquire() {
-        if(Thread.currentThread() != this.lockingThread)
+        if(counter == 0 || Thread.currentThread() != this.lockingThread)
         {
-            while(is_locked.compareAndSet(false, true)
+            while(isLocked.compareAndSet(false, true)
                     && Thread.currentThread() != this.lockingThread)
             {
                 try
                 {
-                    Thread.sleep(10);
-                } catch(InterruptedException e){}//TODO change exception
+                    Thread.sleep(11);
+                } catch(InterruptedException e){
+                }
             }
-           this.lockingThread = Thread.currentThread();
         }
+        this.lockingThread = Thread.currentThread();
         counter++;
     }
 
+    /**
+     * if acquire is possible, locks and returns true
+     * else, returns false
+     * @return true if locking is possible, false if not
+     */
     @Override
     public boolean tryAcquire()
     {
-        if(this.is_locked.compareAndSet(false, true)
-                || Thread.currentThread() == this.lockingThread)
+        if(!isLocked.get() || Thread.currentThread() == this.lockingThread)
         {
             acquire();
             return true;
-        }
-        return false;
+        }else return false;
     }
 
+    /**
+     * releases the current lock, by subtracting 1 from the lock counter.
+     * if locked only once, frees the lock for ant thread.
+     * @throws IllegalReleaseAttempt if lock isn't locked, or if another thread tries to release it
+     */
     @Override
     public void release() {
-        if (Thread.currentThread() != this.lockingThread || this.is_locked.get() == false)
-        {
+        if (counter == 0 || Thread.currentThread()!=this.lockingThread) {
             throw new IllegalReleaseAttempt();
-        }
-        else
-        {
-            if (counter > 1)
+        } else {
+            if (counter == 1)
             {
                 counter--;
+                isLocked.set(false);
             }
-            else
+            else if (counter >1)
             {
                 counter--;
-                is_locked.set(false);
-                this.lockingThread = null;
             }
         }
     }
 
+    /**
+     * releases the lock
+     */
     @Override
     public void close()
     {
