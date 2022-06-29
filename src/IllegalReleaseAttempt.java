e7175926f2b30d0e6524698898a17eb92aff2a22
Bar <bar.muller@campus.technion.ac.il>
2022-06-29 19:37:12 +0300

    e

--- src/IllegalReleaseAttempt.java
+++ src/IllegalReleaseAttempt.java
@@ -1,3 +1,7 @@
+/**
+ * exception for releasing an unlocked lock,
+ * or if a thread that didn't lock the lock tries to release it
+ */
 public class IllegalReleaseAttempt extends IllegalMonitorStateException{
 
     public IllegalReleaseAttempt() {}
